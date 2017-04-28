bbcf0590ed9c015cb9e6eaedd640e92cedcb8829
Benson.zhi <jbzhi@gdrcarbon.com>
2017-04-28 10:59:59 +0800

    update agent_api

--- zabbixapi/agent_api.py
+++ zabbixapi/agent_api.py
@@ -10,7 +10,7 @@ from .models import *
 
 def get_agent_cpu_data(request):
     if request.method == 'GET':
-        hostlist = ['192.168.1.1', '192.168.1.2', '192.168.1.3', '192.168.1.6', '192.168.1.7', '192.168.1.8', '192.168.1.9']
+        hostlist = ['192.168.1.1', '127.0.0.1', '192.168.1.3', '192.168.1.6', '192.168.1.7', '192.168.1.8', '192.168.1.9']
         os.chdir('/usr/local/zabbix/bin')
         for host in hostlist:
             args1 = './zabbix_get -s %s -p 10050 -k "system.cpu.util[,system]"' % host
@@ -22,14 +22,14 @@ def get_agent_cpu_data(request):
             args7 = './zabbix_get -s %s -p 10050 -k "system.cpu.util[,steal]"' % host
             args8 = './zabbix_get -s %s -p 10050 -k "system.cpu.util[,interrupt]"' % host
 
-            system = os.popen(args1).read()
-            user = os.popen(args2).read()
-            idle = os.popen(args3).read()
-            iowait = os.popen(args4).read()
-            nice = os.popen(args5).read()
-            softirq = os.popen(args6).read()
-            steal = os.popen(args7).read()
-            interrupt = os.popen(args8).read()
+            system = os.popen(args1).read().strip()
+            user = os.popen(args2).read().strip()
+            idle = os.popen(args3).read().strip()
+            iowait = os.popen(args4).read().strip()
+            nice = os.popen(args5).read().strip()
+            softirq = os.popen(args6).read().strip()
+            steal = os.popen(args7).read().strip()
+            interrupt = os.popen(args8).read().strip()
 
             cpustat.objects.create(system=system, user=user, idle=idle, iowait=iowait, nice=nice, \
                                 softirq=softirq, steal=steal, interrupt=interrupt)
