8b9956bc93a2e1e71b229b13900daede7d4911ce
Benson.zhi <jbzhi@gdrcarbon.com>
2017-05-03 12:37:24 +0800

    add django_wsgi

--- django_wsgi.py
+++ django_wsgi.py
@@ -10,7 +10,7 @@ sys.setdefaultencoding('utf8')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deploySystem.settings")
 
-# from django.core.wsgi import get_wsgi_application
-from django.core.handlers.wsgi import WSGIHandler
-# application = get_wsgi_application()
-application = WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+# from django.core.handlers.wsgi import WSGIHandler
+application = get_wsgi_application()
+# application = WSGIHandler()
